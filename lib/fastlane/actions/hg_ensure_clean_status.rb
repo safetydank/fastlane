a0646f15f73eec4ae252d99df41bfdf5592b5098
Dan V <danv@adapptor.com.au>
2015-08-25 14:16:43 +0800

    Optionally ignore untracked files

--- lib/fastlane/actions/hg_ensure_clean_status.rb
+++ lib/fastlane/actions/hg_ensure_clean_status.rb
@@ -6,7 +6,13 @@ module Fastlane
     # Raises an exception and stop the lane execution if the repo is not in a clean state
     class HgEnsureCleanStatusAction < Action
       def self.run(params)
-        repo_clean = `hg status -mard`.empty?
+        options = {
+          ignore_untracked: true
+        }.merge(params || {})
+
+        cmd = if options[:ignore_untracked] then 'hg status -mard' else 'hg status' end
+
+        repo_clean = cmd.empty?
 
         if repo_clean
           Helper.log.info 'Mercurial status is clean, all good! 😎'.green
@@ -23,6 +29,12 @@ module Fastlane
       def self.output
         [
           ['HG_REPO_WAS_CLEAN_ON_START', 'Stores the fact that the hg repo was clean at some point']
+        ]
+      end
+
+      def self.available_options
+        [
+          ['ignore_untracked', 'Ignore untracked files?']
         ]
       end
 
